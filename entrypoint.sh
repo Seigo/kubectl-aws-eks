803002ad539868dc20496ede90acb613c509e58a
Guilherme Seigo Osawa <guilhermeosawa@gmail.com>
2021-12-22 16:45:36 -0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-echo "$KUBE_CONFIG_DATA"
+echo "asdasdasd $KUBE_CONFIG_DATA"
 
 echo "$KUBE_CONFIG_DATA" | base64 -d > /tmp/config
 export KUBECONFIG=/tmp/config
+
+sh -c "kubectl config current-context"
 
 sh -c "kubectl $*"
